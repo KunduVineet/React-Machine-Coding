67e1942de076bdbd6ab195447ad059527a7fb9a7
VineetKundu <kunduvineet6@gmail.com>
2025-05-18 20:42:06 +0530

    useMemo() code understanding

--- useMemo/src/App.jsx
+++ useMemo/src/App.jsx
@@ -1,15 +1,29 @@
-import { useState } from 'react'
+import { useMemo, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
+  function expensiveComputation(num) {
+    console.log("inside expensive task called");
+    for(let i = 0; i< 10000000000; i++) {}
+    return num += 2;
+  }
+  let memoizedValue = useMemo(() => { expensiveComputation(5) }, []);
   return (
     <>
       <div className="card text-xl">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          Increment
         </button>
+      </div>
+
+      <div>
+        Count : {count}
+      </div>
+
+      <div>
+        <h1 className="text-2xl">Memoized Value: {memoizedValue}</h1>
       </div>
     </>
   )
