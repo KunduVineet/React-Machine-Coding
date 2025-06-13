2237fc8feef8ca0dd1bc33a197cd4595118addd4
VineetKundu <kunduvineet6@gmail.com>
2025-06-13 10:27:34 +0530

    Pure Components

---
+++ Pure-Component/src/App.jsx
@@ -0,0 +1,17 @@
+import { Component } from 'react'
+import './App.css'
+import PureComp from './PureComp'
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>React App</h1>
+        <PureComp />
+      </div>
+    )
+  }
+  
+}
+
+export default App
---
+++ Pure-Component/src/PureComp.js
@@ -0,0 +1,11 @@
+import React, { PureComponent } from 'react'
+
+export class PureComp extends PureComponent {
+  render() {
+    return (
+      <div>PureCom</div>
+    )
+  }
+}
+
+export default PureComp
